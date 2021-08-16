b157ce7c217b6f93090560f02c81e4eedfc03f5a
Rajat Kumar <rjkumar1308@gmail.com>
2021-08-16 11:57:12 +0530

    added gradient border on stories

--- insta-reactjs/src/components/stories-component/story-component/story.component.jsx
+++ insta-reactjs/src/components/stories-component/story-component/story.component.jsx
@@ -2,7 +2,7 @@ import './story.component.scss';
 
 export const StoryComponent = ({ story }) => (
     <div className="story">
-        <img src={story.avatar} alt="Story" />
+        <div className="module-border-wrap"><img src={story.avatar} alt="Story" /></div>
         <span className="user-name">{story.user_name}</span>
     </div>
 );
